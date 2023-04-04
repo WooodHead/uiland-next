a1c5560c37f959b95dd84a50129f0379af188123
kingebere <gudoski30@gmail.com>
2023-04-05 00:12:49 +0100

    refactor:add comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,13 +11,11 @@ import { useRouter } from 'next/router';
 import * as gtag from '../lib/gtag';
 import { UserContextProvider } from '../context/authContext';
 import { ScreensContextProvider } from '../context/screensContex';
-import Head from 'next/head';
+
 import Header from '../components/Header';
-import ScreenContextProvider from '../context/screenContext';
+
 import { Session } from '@supabase/auth-helpers-react';
 import { AppProps } from 'next/app';
-import { supabase } from '../supabase';
-import { PopContextProvider } from '../context/PopContext';
 
 export default function App({
 	Component,
@@ -38,6 +36,7 @@ export default function App({
 		};
 	}, [router.events]);
 
+	//This runs the hotjar script
 	useEffect(() => {
 		hotjar.initialize(3322744, 6, false);
 	}, []);
@@ -51,17 +50,18 @@ export default function App({
 		>
 			<UserContextProvider>
 				<ScreensContextProvider>
+					{/* script to embed Google Ads */}
 					<Script
 						data-ad-client='ca-pub-5952673528545779'
 						async
 						src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
 					></Script>
-
+					{/* script to embed Google tags */}
 					<Script
 						strategy='afterInteractive'
 						src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
 					/>
-
+					{/* script to embed Google analytics */}
 					<Script id='google-analytics' strategy='afterInteractive'>
 						{`
 				  window.dataLayer = window.dataLayer || [];
@@ -71,6 +71,7 @@ export default function App({
 				  gtag('config', '${gtag.GA_TRACKING_ID}');
 					`}
 					</Script>
+					{/* script to embed Segment */}
 					<Script id='segment' strategy='afterInteractive'>
 						{`  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="CzMF6ZIUJcE8l8lgVpjsbJv1gT5jVvBo";;analytics.SNIPPET_VERSION="4.15.3";
 		analytics.load("CzMF6ZIUJcE8l8lgVpjsbJv1gT5jVvBo");
