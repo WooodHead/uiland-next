123bc933131f74cc0d4f7cc275079552a5f28aaa
codewarsfx <chiderainnocent001@gmail.com>
2023-04-10 12:43:11 +0100

    fix:back to home pill

--- components/ViewMore/index.tsx
+++ components/ViewMore/index.tsx
@@ -6,11 +6,10 @@ import ScreensInCategory from '../ScreensInCategory';
 import { BsArrowLeft } from 'react-icons/bs';
 import Link from 'next/link';
 const Viewmore = ({ screens }) => {
-
 	//get screens from the server then format on the client
 	const [formattedScreen, setFormattedScreen] = useState(screens);
 
-	//function for formatting the screens received i.e choosing random brands from the same category if screens is an array of screens or choosing random brands of different categories if screens is an empty array 
+	//function for formatting the screens received i.e choosing random brands from the same category if screens is an array of screens or choosing random brands of different categories if screens is an empty array
 	const randomScreens = async () => {
 		setFormattedScreen(await formatScreens(screens));
 	};
@@ -20,15 +19,17 @@ const Viewmore = ({ screens }) => {
 	}, [screens]);
 	return (
 		<Wrapper>
-			<div className='home'>
-				<BsArrowLeft />
-				<Link href='/'> Back to Home</Link>{' '}
-			</div>
+			<Link href='/'>
+				<div className='home'>
+					<BsArrowLeft className='icon' />
+					Back to Home
+				</div>
+			</Link>{' '}
 			<Header>
 				View Screenshots from other {screens[0] && screens[0].category} Brands
 			</Header>
 			{/* use the same screensIncategory component as the homepage but pass a viewmore prop so it is styled differently */}
-			<ScreensInCategory screens={formattedScreen} viewmore={true} /> 
+			<ScreensInCategory screens={formattedScreen} viewmore={true} />
 		</Wrapper>
 	);
 };
@@ -38,13 +39,17 @@ const Wrapper = styled.div`
 	max-width: 1300px;
 	margin: 5em auto;
 	.home {
-		font-size: 14px;
-		color: #868686;
-		font-weight: 600;
-		display: flex;
+		display: inline-flex;
 		align-items: center;
-		a{
-			margin-left: .5em;
+		background: rgb(0 0 0 / 9%);
+		border-radius: 28px;
+		padding: 0.8em;
+		color: var(--primary-text-black);
+		font-size: 14px;
+			font-weight: 600;
+		.icon {
+			margin-right: 0.5em;
+		
 		}
 	}
 `;
